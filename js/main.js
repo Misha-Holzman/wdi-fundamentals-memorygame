7cd614182420c41510e8d5a1356ddb40356d3cb2
Misha Holzman <mishaholzman@Kazimiras-MacBook-Pro.local>
2018-08-13 00:06:40 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -4,36 +4,40 @@ var cards = ['queen', 'queen', 'king', 'king'];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-var cardTwo = cards[2];
-
-var cardThree = cards[1];
-
-var cardFour = cards[3];
-
-cardsInPlay.push(cardOne);
-
-cardsInPlay.push(cardTwo);
-
-cardsInPlay.push(cardThree);
-
-cardsInPlay.push(cardFour);
-
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardTwo);
-console.log("user flipped " + cardThree);
-console.log("user flipped " + cardFour);
-
-
-if (cardsInPlay.length === 4 && (cardsInPlay[0] === cardsInPlay[2])) {
-	alert("You found a match!");
-} else {
-	console.log("GUESS AGAIN")
-}
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	alert("Sorry, try again.");
+	}
+};
 
 
 
+var flipCard = function(cardID) {
+    checkForMatch();
+    console.log("user flipped " + cards[cardID]);
+    cardsInPlay.push(cards[cardID]);
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+    
+
+
+
+/* 	if (cardsInPlay.length === 4 && (cardsInPlay[0] === cardsInPlay[2])) {
+	    alert("You found a match!");
+    } else {
+	console.log("Sorry, try again.");
+    }
+
+    (I am unclear if this code should be deleted or not)
+*/
 
 
 
